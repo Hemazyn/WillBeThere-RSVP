01dce59bf04aadb166791d02af8874118d493684
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2024-04-18 20:06:31 +0100

    Add: install tailwindcss and setup login and signup page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-color-white"> HomePage </h1>
+    </div>
+  );
+};
+
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      primary: {
+        light: '#C782F1',
+        DEFAULT: '#A089D7',
+        },
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
