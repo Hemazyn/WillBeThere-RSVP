35305e9a4bd1ead360eae877f0e8aec5b7f6ed59
Andrew Ezeani <ezeaniiandrew@gmail.com>
2024-04-23 20:37:35 +0100

    feat: created user account page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home, Browse, CreateEvent } from './pages';
+import { Home, Browse, CreateEvent, UserAccount } from './pages';
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="browse" element={<Browse />} />
         <Route path="create" element={<CreateEvent />} />
+        <Route path="account" element={<UserAccount />} />
       </Route>
     </Routes>
   )
