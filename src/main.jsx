88551518bc323c52b8f16035a485d102f84fe385
Bibest <bibhestee@gmail.com>
2024-04-24 14:00:43 +0100

    refactor(dev): Remove unimportant comments.

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { all } from '@awesome.me/kit-KIT_CODE/icons'
-
-// library.add(...all)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
