d7ba878c195459fb8afff9f1b32c8bde3a446bc7
Andrew Ezeani <ezeaniiandrew@gmail.com>
2024-04-25 09:48:14 +0100

    updated the UI

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,16 +1,25 @@
+import { Link } from "react-router-dom";
 import Button from "../../components/Button";
 import styles from "./landing.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpFromBracket, faMagnifyingGlass, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function Landing() {
   return (
     <div className={styles.landing}>
+            <nav>
+                <div className={styles.logo}>
+        <Link to="/">will be there</Link>
+      </div>
+            </nav>
       <p>
         Events&activities <span>just near you!</span>
       </p>
       <div className={styles.btns}>
-        <Button>create events</Button>
-        <Button>search for activities</Button>
-        <Button>share meetings</Button>
+        <Button>create events<FontAwesomeIcon icon={faPlus} /></Button>
+        <Button>search for activities
+          <FontAwesomeIcon icon={faMagnifyingGlass} /></Button>
+        <Button>share meetings <FontAwesomeIcon icon={faArrowUpFromBracket} /></Button>
         <Button>join events</Button>
       </div>
       <Button>get started</Button>
