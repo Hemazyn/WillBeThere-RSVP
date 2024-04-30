10323d2239268722073c8e023b0c4ea6bd797afd
Leeyah-123 <junaidaaliyah260@gmail.com>
2024-04-30 22:07:06 +0100

    feat: Google Login and Signup

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,3 +1,5 @@
+import { GoogleLogin } from '@react-oauth/google';
+import { jwtDecode } from 'jwt-decode';
 import { Loading, Notify } from 'notiflix';
 import { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
@@ -39,6 +41,15 @@ const Login = () => {
     }
 
     login(form);
+  };
+
+  const handleGoogleOneTapLogin = async (codeResponse) => {
+    const userDetails = jwtDecode(codeResponse.credential);
+
+    login({
+      username: userDetails.name.replace(' ', ''),
+      password: userDetails.sub,
+    });
   };
 
   useEffect(() => {
@@ -116,39 +127,7 @@ const Login = () => {
         {/* icons */}
         <div className="flex flex-col w-full items-center gap-5 md:gap-10 px-5">
           <div className="flex flex-row font-Bayon gap-[60px]">
-            <Link to="">
-              <svg
-                className="w-10 h-10 md:w-15 md:h-15"
-                viewBox="0 0 60 60"
-                fill="white"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <g>
-                  <rect width="60" height="60" rx="10" />
-                  <path
-                    d="M41.7666 27.6498H40.8V27.6H30V32.4H36.7818C35.7924 35.1942 33.1338 37.2 30 37.2C26.0238 37.2 22.8 33.9762 22.8 30C22.8 26.0238 26.0238 22.8 30 22.8C31.8354 22.8 33.5052 23.4924 34.7766 24.6234L38.1708 21.2292C36.0276 19.2318 33.1608 18 30 18C23.373 18 18 23.373 18 30C18 36.627 23.373 42 30 42C36.627 42 42 36.627 42 30C42 29.1954 41.9172 28.41 41.7666 27.6498Z"
-                    fill="#FFC107"
-                  />
-                  <path
-                    d="M19.3835 24.4146L23.3261 27.306C24.3929 24.6648 26.9765 22.8 29.9999 22.8C31.8353 22.8 33.5051 23.4924 34.7765 24.6234L38.1707 21.2292C36.0275 19.2318 33.1607 18 29.9999 18C25.3907 18 21.3935 20.6022 19.3835 24.4146Z"
-                    fill="#FF3D00"
-                  />
-                  <path
-                    d="M30 41.9999C33.0996 41.9999 35.916 40.8136 38.0454 38.8846L34.3314 35.7419C33.1266 36.6544 31.629 37.1999 30 37.1999C26.8788 37.1999 24.2286 35.2097 23.2302 32.4323L19.317 35.4472C21.303 39.3334 25.3362 41.9999 30 41.9999Z"
-                    fill="#4CAF50"
-                  />
-                  <path
-                    d="M41.7666 27.6499H40.8V27.6001H30V32.4001H36.7818C36.3066 33.7423 35.4432 34.8997 34.3296 35.7427C34.3302 35.7421 34.3308 35.7421 34.3314 35.7415L38.0454 38.8843C37.7826 39.1231 42 36.0001 42 30.0001C42 29.1955 41.9172 28.4101 41.7666 27.6499Z"
-                    fill="#1976D2"
-                  />
-                </g>
-                <defs>
-                  <clipPath id="clip0_42_1105">
-                    <rect width="60" height="60" fill="white" />
-                  </clipPath>
-                </defs>
-              </svg>
-            </Link>
+            <GoogleLogin onSuccess={handleGoogleOneTapLogin} useOneTap />
             <button type="button" onClick={() => Notify.info('Coming Soon')}>
               <svg
                 className="w-10 h-10 md:w-15 md:h-15"
